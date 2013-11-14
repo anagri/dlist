36e648865478d35edb22d296ee7fea750105c34f
Amir <amir.nagri+git@gmail.com>
2013-11-14 16:14:57 +0530

    rails scaffold

---
+++ dlist-web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DlistWeb::Application.config.secret_token = 'e2d9d4523bfe96d505ae76f2b2d89d155ade0e378f5cb6243af32d281b03c9bafb67889bd47cf185354f4f10d080a30583e0f9ee08af23ee4d33b6dba8ef7bd8'
