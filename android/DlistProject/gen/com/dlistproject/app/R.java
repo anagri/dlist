252ce2d62190f1a5e811ac211ba085fffa3e0640
NitinGK <nitin.gk@gmail.com>
2013-11-15 02:27:52 +0530

    display is working

--- android/DlistProject/gen/com/dlistproject/app/R.java
+++ android/DlistProject/gen/com/dlistproject/app/R.java
@@ -24,18 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int acceptButtonView=0x7f080005;
-        public static final int action_settings=0x7f080006;
-        public static final int creatorNameView=0x7f080003;
-        public static final int descView=0x7f080002;
-        public static final int parentLayout=0x7f080000;
-        public static final int priorityTextView=0x7f080001;
-        public static final int seperatorView=0x7f080004;
+        public static final int acceptButtonView=0x7f080008;
+        public static final int action_add=0x7f080009;
+        public static final int creatorNameView=0x7f080006;
+        public static final int descView=0x7f080005;
+        public static final int listCardsView=0x7f080000;
+        public static final int newTastContentView=0x7f080001;
+        public static final int parentLayout=0x7f080003;
+        public static final int priorityTextView=0x7f080004;
+        public static final int seperatorView=0x7f080007;
+        public static final int setTaskButtonView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_auth=0x7f030000;
         public static final int activity_landing=0x7f030001;
-        public static final int individual_card=0x7f030002;
+        public static final int add_new_task=0x7f030002;
+        public static final int individual_card=0x7f030003;
     }
     public static final class menu {
         public static final int auth=0x7f070000;
--- android/DlistProject/src/com/dlistproject/app/AuthActivity.java
+++ android/DlistProject/src/com/dlistproject/app/AuthActivity.java
@@ -1,27 +1,50 @@
 package com.dlistproject.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dlistproject.app.CsvReadHelper.TodoTasksArray;
 import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxFile;
+import com.dropbox.sync.android.DbxFileSystem;
+import com.dropbox.sync.android.DbxPath;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class AuthActivity extends Activity {
 	private DbxAccountManager mDbxAcctMgr;
-	private static final String APP_KEY = "vyc3fsqjpems5dl";
-	private static final String APP_SECRET = "ozgej4db0tyyc0f";
+	TodoTasksArray todoTaskArray;
+	String descData = "";
+	private static final String APP_KEY = "4ul0p4hmwzmk9ah";
+	private static final String APP_SECRET = "po67tpsyy9mv3lh";
 	static final int REQUEST_LINK_TO_DBX = 0;
+	ListView listCardsViewObj;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_auth);
 //		startActivity(new Intent(AuthActivity.this,LandingActivity.class));
+		listCardsViewObj = (ListView) findViewById(R.id.listCardsView);
 		mDbxAcctMgr = DbxAccountManager.getInstance(getApplicationContext(), APP_KEY, APP_SECRET);
-		mDbxAcctMgr.startLink((Activity)this, REQUEST_LINK_TO_DBX);
-    	Log.i("TAG","oncreate");
+		if(!mDbxAcctMgr.hasLinkedAccount()) {
+			mDbxAcctMgr.startLink((Activity)AuthActivity.this, REQUEST_LINK_TO_DBX);
+		}else {
+			setup_file();
+		}
+		Log.i("TAG","oncreate");
+        
 		
 	}
 	@Override
@@ -29,7 +52,7 @@ public class AuthActivity extends Activity {
 	    if (requestCode == REQUEST_LINK_TO_DBX) {
 	        if (resultCode == Activity.RESULT_OK) {
 	        	Log.i("TAG","worked");
-	            Toast.makeText(getApplicationContext(), "worked", Toast.LENGTH_SHORT).show();
+	        	setup_file();
 	        } else {
 	        	Log.i("TAG","failed");
 	            Toast.makeText(getApplicationContext(), "failed", Toast.LENGTH_SHORT).show();
@@ -38,6 +61,63 @@ public class AuthActivity extends Activity {
 	        super.onActivityResult(requestCode, resultCode, data);
 	    }
 	}
+	public boolean setup_file() {
+        try {
+        	DbxFileSystem dbxFs = DbxFileSystem.forAccount(mDbxAcctMgr.getLinkedAccount());
+        	if(dbxFs.exists(new DbxPath("todo_items.csv"))) {
+        		DbxFile csvFile = dbxFs.open(new DbxPath("todo_items.csv"));
+        		Toast.makeText(getApplicationContext(), "file exists",Toast.LENGTH_SHORT).show();
+        		CsvReadHelper csvHelper = new CsvReadHelper();
+        	    todoTaskArray = csvHelper.getTodoList(csvFile);
+        	    CustomAdapter adapter = new CustomAdapter(getApplicationContext(), todoTaskArray);
+        	    Log.i("TAG","setting adapter");
+        	    listCardsViewObj.setAdapter(adapter);
+        	    csvFile.close();
+        	}else {
+        		DbxFile testFile = dbxFs.create(new DbxPath("todo_items.csv"));
+        		Toast.makeText(getApplicationContext(), "file created", Toast.LENGTH_SHORT).show();
+        	}
+        	return false;
+        }catch(Exception e) {
+        	Log.i("TAG","exception in DBXfile sysytem "+e.toString());
+        }
+        return true;
+
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.action_add) {
+			showCustomDialog();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	protected void showCustomDialog() {
+        // TODO Auto-generated method stub
+        final Dialog dialog = new Dialog(AuthActivity.this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.add_new_task);
+        
+        final EditText editText = (EditText)dialog.findViewById(R.id.newTastContentView);
+        Button button = (Button)dialog.findViewById(R.id.setTaskButtonView);    
+        button.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+            	String data = editText.getText().toString().trim();
+            	if(!data.equals("")){
+            		TodoTask todoTask =  new TodoTask();
+	            	todoTask.setTitle(data);
+	            	todoTaskArray.insert(todoTask) ;
+            	}else {
+            		Toast.makeText(getApplicationContext(), "No data", Toast.LENGTH_SHORT).show();
+            	}
+                dialog.dismiss();
+                
+            }
+        });
+                
+        dialog.show();
+    }	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
