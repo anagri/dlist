898ad2028cfa8c37e20dc7402f75119d152aebc0
kaushik <kaushik@flipkart.com>
2013-11-15 05:15:47 +0530

    writeString method used insted of outstream

--- android/DlistProject/src/com/dlistproject/app/csv/CsvHelper.java
+++ android/DlistProject/src/com/dlistproject/app/csv/CsvHelper.java
@@ -1,12 +1,9 @@
 package com.dlistproject.app.csv;
 
-import au.com.bytecode.opencsv.CSVWriter;
 import com.dlistproject.app.data.TodoTasksArray;
 import com.dropbox.sync.android.DbxFile;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 
 /**
  * User: kaushik Date: 14/11/13 Time: 10:33 PM
@@ -14,6 +11,33 @@ import java.io.OutputStreamWriter;
 
 public class CsvHelper
 {
+  public static final int INITIAL_STRING_SIZE = 128;
+
+  /** The character used for escaping quotes. */
+  public static final char DEFAULT_ESCAPE_CHARACTER = '"';
+
+  /** The default separator to use if none is supplied to the constructor. */
+  public static final char DEFAULT_SEPARATOR = ',';
+
+  /**
+   * The default quote character to use if none is supplied to the
+   * constructor.
+   */
+  public static final char DEFAULT_QUOTE_CHARACTER = '"';
+
+  /** The quote constant to use when you wish to suppress all quoting. */
+  public static final char NO_QUOTE_CHARACTER = '\u0000';
+
+  /** The escape constant to use when you wish to suppress all escaping. */
+  public static final char NO_ESCAPE_CHARACTER = '\u0000';
+
+  /** Default line terminator uses platform encoding. */
+  public static final String DEFAULT_LINE_END = "\n";
+
+  private char separator = DEFAULT_SEPARATOR;
+  private char quotechar = DEFAULT_QUOTE_CHARACTER;
+  private int escapechar = DEFAULT_ESCAPE_CHARACTER;
+
   public TodoTasksArray getTodoList(DbxFile dbxFile) throws IOException
   {
     return new TodoTasksArray(dbxFile.getReadStream());
@@ -21,21 +45,68 @@ public class CsvHelper
 
   public void writeCsv(DbxFile dbxFile, TodoTasksArray tasksArray) throws IOException
   {
-    FileOutputStream writeStream = dbxFile.getWriteStream();
-	CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(writeStream));
     String[] taskArray = tasksArray.getHeaders().toArray(new String[]{});
-    writeTaskToFile(taskArray,csvWriter);
+    writeTaskToFile(taskArray,dbxFile);
 
     for (int i = 0; i < tasksArray.getCount(); i++)
     {
-      writeTaskToFile(tasksArray.getPosition(i).toStringArray(), csvWriter);
+      writeTaskToFile(tasksArray.getPosition(i).toStringArray(), dbxFile);
     }
-    writeStream.flush();
-    writeStream.close();
   }
 
-  private void writeTaskToFile(String[] taskArray, CSVWriter csvWriter)
+  private void writeTaskToFile(String[] taskArray, DbxFile dbxFile) throws IOException
   {
-    csvWriter.writeNext(taskArray);
+    dbxFile.writeString(getCsvLine(taskArray));
+  }
+
+
+  public String getCsvLine(String[] nextLine)
+  {
+    if (nextLine == null)
+      return null;
+
+    StringBuilder sb = new StringBuilder(INITIAL_STRING_SIZE);
+    for (int i = 0; i < nextLine.length; i++) {
+
+      if (i != 0) {
+
+        sb.append(separator);
+      }
+
+      String nextElement = nextLine[i];
+      if (nextElement == null)
+        continue;
+      if (quotechar !=  NO_QUOTE_CHARACTER)
+        sb.append(quotechar);
+
+      sb.append(stringContainsSpecialCharacters(nextElement) ? processLine(nextElement) : nextElement);
+
+      if (quotechar != NO_QUOTE_CHARACTER)
+        sb.append(quotechar);
+    }
+
+    sb.append(DEFAULT_LINE_END);
+    return sb.toString();
+  }
+
+  private boolean stringContainsSpecialCharacters(String line) {
+    return line.indexOf(quotechar) != -1 || line.indexOf(escapechar) != -1;
+  }
+
+  private StringBuilder processLine(String nextElement)
+  {
+    StringBuilder sb = new StringBuilder(INITIAL_STRING_SIZE);
+    for (int j = 0; j < nextElement.length(); j++) {
+      char nextChar = nextElement.charAt(j);
+      if (escapechar != NO_ESCAPE_CHARACTER && nextChar == quotechar) {
+        sb.append(escapechar).append(nextChar);
+      } else if (escapechar != NO_ESCAPE_CHARACTER && nextChar == escapechar) {
+        sb.append(escapechar).append(nextChar);
+      } else {
+        sb.append(nextChar);
+      }
+    }
+
+    return sb;
   }
 }
