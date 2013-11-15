960ab4cfc4718d6303dbf029c5c8d5d41dbc5ac3
NitinGK <nitin.gk@gmail.com>
2013-11-15 06:11:35 +0530

    display working and write working

--- android/DlistProject/gen/com/dlistproject/app/R.java
+++ android/DlistProject/gen/com/dlistproject/app/R.java
@@ -25,15 +25,17 @@ public final class R {
     }
     public static final class id {
         public static final int acceptButtonView=0x7f080008;
-        public static final int action_add=0x7f080009;
+        public static final int action_add=0x7f08000b;
         public static final int creatorNameView=0x7f080006;
         public static final int descView=0x7f080005;
         public static final int listCardsView=0x7f080000;
+        public static final int locationView=0x7f080009;
         public static final int newTastContentView=0x7f080001;
         public static final int parentLayout=0x7f080003;
         public static final int priorityTextView=0x7f080004;
         public static final int seperatorView=0x7f080007;
         public static final int setTaskButtonView=0x7f080002;
+        public static final int statusView=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_auth=0x7f030000;
